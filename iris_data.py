f003c770d47d29663f28f92c0b1c14a7d6b82479
David Jones <davidsamueljones@icloud.com>
2018-11-21 18:20:02 +0000

    Added stub for q3+
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

---
+++ iris_data.py
@@ -0,0 +1,12 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import scipy.stats as sps
+from mpl_toolkits.mplot3d import Axes3D
+
+####################################################
+# MAIN
+####################################################
+if __name__ == "__main__":
+    print("MAIN")
