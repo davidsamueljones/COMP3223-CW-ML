3dd0dae32a94a6eeb1f7fd12f5147d041996685f
David Jones <davidsamueljones@icloud.com>
2018-11-22 23:55:31 +0000

    Added stub for section 4
    Signed-off-by: David Jones <davidsamueljones@icloud.com>

---
+++ linear_reg.py
@@ -0,0 +1,12 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy as spy
+import scipy.linalg
+
+####################################################
+# MAIN
+####################################################
+if __name__ == "__main__":
+    print("MAIN")
